68e07eddb7f981643bc6f71429d0d7a68804992b
harish <atpcs033@outlook.com>
2026-05-19 12:41:49 +0530

    ci/cd-rollback

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    
+    syntax error here
 });
 
 Route::get('/dashboard', function () {
